46fca5081797a2e9d3bbdec8377cab2552028965
anuj8918 <anujm8918gmail.com>
2025-06-03 00:39:04 +0530

    changes done

--- server/server.js
+++ server/server.js
@@ -14,13 +14,13 @@ const allowedOrigins = ['http://localhost:5173']
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 //API Endpoints
-app.get("/",(req,res)=> res.send("API working"));
+app.get("/", (req, res) => res.send("API working"));
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Server started on PORT:${port}`);
-})
+});
